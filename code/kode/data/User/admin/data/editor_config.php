3cf08e042d54ee781e63015c04f342381c37ff2d
wangjiang <wangjiang988@163.com>
2018-05-08 12:55:40 +0800

    加上离线设置

---
+++ code/kode/data/User/admin/data/editor_config.php
@@ -0,0 +1,12 @@
+<?php exit;?>{
+    "fontSize": "14px",
+    "theme": "tomorrow",
+    "autoWrap": 1,
+    "autoComplete": 1,
+    "functionList": 1,
+    "tabSize": 4,
+    "softTab": 1,
+    "displayChar": 0,
+    "fontFamily": "Menlo",
+    "keyboardType": "ace"
+}
